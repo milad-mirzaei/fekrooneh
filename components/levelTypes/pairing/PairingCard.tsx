dcccf7fe81436a77059a48bed8ff962589287bb9
MilAd MirzAei <miladmirzaei6802@gmail.com>
2023-06-05 20:56:35 +0330

    2

--- components/levelTypes/pairing/PairingCard.tsx
+++ components/levelTypes/pairing/PairingCard.tsx
@@ -22,13 +22,13 @@ const PairingCard:React.FC<PairingCardProps> = ({item,index}) => {
 
 
   return (
-    <div className={`w-[200px] h-[360px] flex hover:scale-105 transition-all duration-300
-    ${pairingItems.length == 2 && 'w-[420px] h-[210px] flex-row gap-3 p-[14px]'}
-    ${pairingItems.length == 3 && 'w-[315px] h-[205px] flex-row gap-3 p-[13px]'}
+    <div className={` flex hover:scale-105 transition-all duration-300
+    ${pairingItems.length == 2 && 'w-[420px] h-[205px] flex-row gap-3 p-[14px]'}
+    ${pairingItems.length == 3 && 'w-[315px] h-[200px] flex-row gap-3 p-[13px]'}
     ${pairingItems.length == 4 && 'w-[180px] h-[340px] flex-col gap-5 p-[17px]'}
-    ${pairingItems.length == 5 && 'w-[165px] h-[320px] flex-col gap-3 p-[12px]'}
-    ${pairingItems.length == 6 && 'w-[310px] h-[165px] flex-row gap-3 p-[9px]'}
-    ${pairingItems.length == 8 && 'w-[240px] h-[165px] flex-row gap-2 p-[7px]'}
+    ${pairingItems.length == 5 && 'w-[160px] h-[320px] flex-col gap-3 p-[12px]'}
+    ${pairingItems.length == 6 && 'w-[310px] h-[160px] flex-row gap-3 p-[9px]'}
+    ${pairingItems.length == 8 && 'w-[240px] h-[160px] flex-row gap-2 p-[7px]'}
     rounded-[27px] ${item.color} border-[1px] border-black items-center justify-between `} style={{ boxShadow: "2px 2    px black" }}>
        
         <InnerItemCard item={item.innerItems[0]} index={0} parentIndex={index} />
